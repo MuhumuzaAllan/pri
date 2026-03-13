6a2a4aea86fcdb7b8fbe586317daafe0c78a4795
MuhumuzaAllan <156339830+MuhumuzaAllan@users.noreply.github.com>
2026-03-13 09:59:19 +0000

    lets check from the browser

--- nextjs-prisma/next.config.ts
+++ nextjs-prisma/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "zany-halibut-jjjpw96w57ww3jggr-3000.app.github.dev",
+      ],
+    },
+  },
 };
 
 export default nextConfig;
